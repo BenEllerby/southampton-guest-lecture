cc83e726465bd8abe304ecf27e82bf031ad59de8
Ben Ellerby <benellerby@hotmail.co.uk>
2017-10-21 15:28:04 +0100

    added intro message and changed basic language to english

--- client/src/components/Root/Root.component.js
+++ client/src/components/Root/Root.component.js
@@ -25,7 +25,7 @@ export default class Root extends Component {
 
   render() {
     return (
-      <IntlProvider locale="fr" messages={locales.fr} >
+      <IntlProvider locale="en" messages={locales.en} >
         <div className="App">
           <div className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
